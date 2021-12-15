f673053b6119cf80b38f11735a329272574d526b
julio-cesar-aguero <julio.aguero.gomez@gmail.com>
2021-12-14 19:38:20 -0600

    portfolio sin projects

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,27 @@ const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 const toggleColors = document.getElementById('toggle-colors');
 const rootStyles = document.documentElement.style;
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll("[data-section]");
+
+
+flagsElement.addEventListener("click", (e) =>{
+    console.log(e.target.parentElement.dataset.languaje);
+    changeLanguaje(e.target.parentElement.dataset.languaje);
+})
+
+const changeLanguaje = async languaje =>{
+    const requestJson = await fetch(`./languages/${languaje}.json`)
+    const texts = await requestJson.json()
+    console.log(texts)
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value;
+        console.log(section,value);
+        textToChange.innerHTML = texts[section][value];
+        
+    }
+}
 
 toggleTheme.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
